221ef1095002c25d10767ca451912300d826f55a
George Dragan <george.dragan0304@gmail.com>
2017-01-28 21:03:38 +0200

    Working version of Calculator

---
+++ Calculator/calculator_script.js
@@ -0,0 +1,10 @@
+function displayNum(num){
+
+	calcform.display.value = calcform.display.value + num;
+
+}
+
+function calculate(val){
+	calcform.display.value = eval(val);
+
+}
--- place_id_extractor.js
+++
@@ -1,4 +0,0 @@
- function place_id_extractor(){
- 	alert(document.getElementById("place-id").textContent);
- 	
- }
