94a5596578ad40be181af1fdcf6291e2d9cc16c8
George Dragan <george.dragan0304@gmail.com>
2017-02-19 14:44:32 +0200

    Working version of Image_Enlarger

---
+++ Image_Enlarger/image_enlarger_script.js
@@ -0,0 +1,20 @@
+/**
+ * Created by george on 19.02.2017.
+ */
+
+function enlarge(image){
+
+    //var image = document.getElementById(id);
+    //image.setAttribute("style", "height:400px width:400px");
+    image.style.height = "400px";
+    image.style.width= "400px";
+}
+
+function reverseEnlarge(image){
+
+    //var image = document.getElementById(id);
+    // image.setAttribute("style", "height:150px width:150px");
+    image.style.height = "150px";
+    image.style.width= "150px";
+
+}
